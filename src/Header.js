691ad767a2a7673976c4498a56969e86551864fa
Leo Martin <lbmartin@butler.edu>
2017-05-23 09:12:31 -0400

    Created NavLinks Component with map

--- src/Header.js
+++ src/Header.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import './Header.css'
 
+const links = ['home', 'about', 'login']
+
 class Header extends Component {
   render() {
     return (
@@ -9,11 +11,7 @@ class Header extends Component {
           <div className="float-left logo">
             <img src="http://www.iconninja.com/files/409/4/1006/axe-icon.svg" alt="axe" />
           </div>
-          <div className="float-right nav-links">
-            <a href="#">Home</a>
-            <a href="#">About</a>
-            <a href="#">Log in</a>
-          </div>
+          <NavLinks links={links}/>
         </div>
         <div className="headline text-center small-12 columns">
           <h1>The Dwarf Underground</h1>
