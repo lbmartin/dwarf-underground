e86ace1710f35fb79015970f067f22f554583e37
Leo Martin <lbmartin@butler.edu>
2017-05-23 10:05:13 -0400

    Fixed props link

--- src/NavLinks.js
+++ src/NavLinks.js
@@ -5,7 +5,7 @@ const links = ['home', 'about', 'login']
 
 class NavLinks extends Component {
   render() {
-      const links = this.props.links.map(link => <a href="#">(link)</a>)
+      const links = this.props.links.map((link,i) => <a href="#" key={i}>(link)</a>)
     return (
           <div className="float-right nav-links">
            {links}
